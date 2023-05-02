4095eed92167a04dcb569a4e30163668f273e9e6
Nat10k <110515021+Nat10k@users.noreply.github.com>
2023-05-02 18:51:23 +0700

    fix : change inventory listBarang to List<Barang>

--- src/main/java/main/Barang/Inventory.java
+++ src/main/java/main/Barang/Inventory.java
@@ -5,46 +5,45 @@ package main.Barang;
  */
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Collections;
 
 public class Inventory {
-    private HashMap<Integer,Barang> listBarang;
+    private ArrayList<Barang> listBarang;
     private int lastID;
 
     // CONSTRUCTOR
     public Inventory() {
-        this.listBarang = new HashMap<Integer,Barang>();
+        this.listBarang = new ArrayList<Barang>();
         this.lastID = 0;
     }
-    public Inventory(HashMap<Integer,Barang> Map) {
-        this.listBarang = Map;
-        this.lastID = Collections.max(Map.keySet());
+    public Inventory(ArrayList<Barang> list) {
+        this.listBarang = list;
     }
 
     // GETTER
-    public HashMap<Integer,Barang> getMapBarang() {
+    public ArrayList<Barang> getListBarang() {
         return this.listBarang;
     }
-    public List<Barang> getListBarang() {
-        List<Barang> temp = new ArrayList<Barang>();
-        for (Barang b : this.listBarang.values()) {
-            temp.add(b);
-        }
-        return temp;
-    }
     public Barang getBarangByID(int ID) {
-        return this.listBarang.get(ID);
+        for (Barang b : this.listBarang) {
+            if (b.getID() == ID) {
+                return b;
+            }
+        }
+        return null;
     }
 
     // PENAMBAHAN/PENGURANGAN BARANG
     public void addBarang(Barang b) {
         b.setID(this.lastID);
-        this.listBarang.put(this.lastID, b);
+        this.listBarang.add(b);
         this.lastID++;
     }
     public void deleteBarang(int ID) {
-        this.listBarang.remove(ID);
+        for (Barang b : this.listBarang) {
+            if (b.getID() == ID) {
+                this.listBarang.remove(b);
+                return;
+            }
+        }
     }
 }
