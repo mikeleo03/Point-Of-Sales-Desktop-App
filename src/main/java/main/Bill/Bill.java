cec57451d55b618af8f0aba50a0c087103347c3a
SaktiWidyatmaja <saktiwidyatmaja1@gmail.com>
2023-05-05 06:11:10 +0700

    feat: add addItem method to Bill

--- src/main/java/main/Bill/Bill.java
+++ src/main/java/main/Bill/Bill.java
@@ -1,10 +1,11 @@
 package main.Bill;
 
 import main.Transaksi.DetailTransaksi;
+import main.Barang.Inventory;
 
 /**
  * File : Bill.java
- * Class untuk menyimpan data fixed bill
+ * Class untuk menyimpan data bill sementara
  */
 
 public class Bill extends Recap {
@@ -12,7 +13,13 @@ public class Bill extends Recap {
         super(idCustomer, waktu, tanggal, detailTransaksi, nominal);
     }
 
-    public void addItem() {
-        
+    public void addItem(Integer idBarang, Integer jumlahBarang, Inventory inventory) {
+        DetailTransaksi dT = this.getDetailTransaksi();
+        dT.addBarang(idBarang, jumlahBarang, inventory);
+        // this.setDetailTransaksi(dT);
     }
+
+    // public void removeItem(Integer idBarang, Integer jumlahBarang, Inventory inventory) {
+    //     detailTransaksi.addBarang(idBarang, jumlahBarang, inventory);
+    // }
 }
