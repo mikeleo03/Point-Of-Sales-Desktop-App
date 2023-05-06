52f122f46bfb838682ea5e636463bb6125d69307
Nat10k <110515021+Nat10k@users.noreply.github.com>
2023-05-06 17:10:27 +0700

    add : updateBarang method

--- src/main/java/main/Barang/Inventory.java
+++ src/main/java/main/Barang/Inventory.java
@@ -20,6 +20,7 @@ public class Inventory implements Serializable {
     }
 
     public Inventory(ArrayList<Barang> list) {
+        // Konstruktor untuk masukan dari file
         this.listBarang = list;
         this.lastID = 0;
         for (Barang b : list) {
@@ -32,21 +33,26 @@ public class Inventory implements Serializable {
     // GETTER
     @XmlAttribute
     public Integer getLastID() {
+        // Getter lastID
         return this.lastID;
     }
     public void setLastID(Integer lastID) {
+        // Setter lastID
         this.lastID = lastID;
     }
 
     @XmlElement
     public ArrayList<Barang> getListBarang() {
+        // Getter listBarang
         return this.listBarang;
     }
     public void setListBarang(ArrayList<Barang> listBarang) {
+        // Setter listBarang
         this.listBarang = listBarang;
     }
 
     public Barang getBarangByID(int ID) {
+        // Mengembalikan barang dengan ID yang diminta
         for (Barang b : this.listBarang) {
             if (b.getID() == ID) {
                 return b;
@@ -57,15 +63,42 @@ public class Inventory implements Serializable {
 
     // PENAMBAHAN/PENGURANGAN BARANG
     public void addBarang(Barang b) {
+        // Menambah barang baru
         b.setID(this.lastID);
         this.listBarang.add(b);
         this.lastID++;
     }
 
     public void deleteBarang(int ID) {
-        for (Barang b : this.listBarang) {
-            if (b.getID() == ID) {
-                this.listBarang.remove(b);
+        // Menghapus barang dengan ID yang diberikan
+        for (int i=0; i<this.listBarang.size(); i++) {
+            if (this.listBarang.get(i).getID() == ID) {
+                this.listBarang.remove(this.listBarang.get(i));
+            }
+        }
+    }
+
+    public void updateBarang(int ID, String name, Integer stock, Double price, Double buyPrice, String category, String picturePath) {
+        // Update barang dengan data baru. Atribut yang tidak ingin diupdate diberi parameter null
+        Barang b = this.getBarangByID(ID);
+        if (b != null) {
+            if (name != null) {
+                b.setName(name);
+            }
+            if (stock != null) {
+                b.setStock(stock);
+            }
+            if (price != null) {
+                b.setPrice(price);
+            }
+            if (buyPrice != null) {
+                b.setBuyPrice(buyPrice);
+            }
+            if (category != null) {
+                b.setCategory(category);
+            }
+            if (picturePath != null) {
+                b.setPicturePath(picturePath);
             }
         }
     }
