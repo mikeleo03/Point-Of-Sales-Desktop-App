a25be6141148c9c92d2c727510ddd7c733b72279
AustinPardosi <gabrielpardosi26@gmail.com>
2023-05-06 19:43:59 +0700

    fix: Sistem Laporan

--- src/main/java/main/Laporan/LaporanFixedBill.java
+++ src/main/java/main/Laporan/LaporanFixedBill.java
@@ -1,5 +1,6 @@
 package main.Laporan;
 
+import java.io.Console;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -18,37 +19,38 @@ import be.quodlibet.boxable.Cell;
 import be.quodlibet.boxable.Row;
 import be.quodlibet.boxable.image.Image;
 import be.quodlibet.boxable.utils.PDStreamUtils;
+import be.quodlibet.boxable.utils.PageContentStreamOptimized;
 import main.Barang.Barang;
 
 public class LaporanFixedBill extends Laporan{
-    private ArrayList<Barang> listPenjualan; 
+    private ArrayList<Barang> listFixedBill; 
 
-    public LaporanFixedBill(ArrayList<Barang> listPenjualan, String fileName) {
+    public LaporanFixedBill(ArrayList<Barang> listFixedBill, String fileName) {
         super(fileName);
-        this.listPenjualan = listPenjualan;
+        this.listFixedBill = listFixedBill;
     }
 
     @Override
-    protected void generatePDF() throws IOException {
+    public void generatePDF() throws IOException {
         PDDocument document = new PDDocument();
         PDPage page = new PDPage(PDRectangle.A4);
         PDPageContentStream contentStream = new PDPageContentStream(document, page);
+        // PageContentStreamOptimized streamOptimized = new PageContentStreamOptimized(contentStream);
         
-        // Image image = new Image(ImageIO.read(new File("/../../img/icon/logo.png")));
+        // Image image = new Image(ImageIO.read(new File(System.getProperty("user.dir") + "\\img\\icon\\images\\logo.png")));
         // float imageWidth = 75;
         // image = image.scaleByWidth(imageWidth);
-        // float xPosition = 50;
-        // float yPosition = page.getMediaBox().getHeight() - image.getHeight() - 50;
-        // image.draw(document, contentStream, xPosition, yPosition);
+        // float xPos = 50;
+        // float yPos = page.getMediaBox().getHeight() - image.getHeight() - 50;
+        // image.draw(document,streamOptimized, xPos, yPos);
 
         contentStream.beginText();
         contentStream.setFont(PDType1Font.HELVETICA_BOLD, 22);
         contentStream.newLineAtOffset(200, 750);
-        contentStream.showText("Laporan FixedBill");
+        contentStream.showText("Laporan Fixed Bill");
         contentStream.endText();
-        contentStream.newLineAtOffset(0, -30);
 
-        float margin = 50;
+        float margin = 40;
         float yStartNewPage = page.getMediaBox().getHeight() - (2 * margin);
         float tableWidth = page.getMediaBox().getWidth() - (2 * margin);
         boolean drawContent = true;
@@ -56,22 +58,28 @@ public class LaporanFixedBill extends Laporan{
         float yPosition = 680;
         BaseTable table = new BaseTable(yPosition, yStartNewPage, bottomMargin, tableWidth, margin, document, page, true, drawContent);
         Row<PDPage> headerRow = table.createRow(15f);
-        Cell<PDPage> cell = headerRow.createCell(10, "No.");
-        cell = headerRow.createCell(100, "Nama Barang");
-        cell = headerRow.createCell(30, "Stock");
-        cell = headerRow.createCell(40, "Harga");
-        cell = headerRow.createCell(40, "Harga Beli");
-        cell = headerRow.createCell(90, "Kategori");
+        Cell<PDPage> cell = headerRow.createCell(5, "No.");
+        cell.setFont(PDType1Font.HELVETICA_BOLD);
+        cell = headerRow.createCell(30, "Nama Barang");
+        cell.setFont(PDType1Font.HELVETICA_BOLD);
+        cell = headerRow.createCell(10, "Stock");
+        cell.setFont(PDType1Font.HELVETICA_BOLD);
+        cell = headerRow.createCell(15, "Harga");
+        cell.setFont(PDType1Font.HELVETICA_BOLD);
+        cell = headerRow.createCell(15, "Harga Beli");
+        cell.setFont(PDType1Font.HELVETICA_BOLD);
+        cell = headerRow.createCell(20, "Kategori");
+        cell.setFont(PDType1Font.HELVETICA_BOLD);
         table.addHeaderRow(headerRow);
         int count = 1;
-        for (Barang penjualan : listPenjualan) {
+        for (Barang penjualanFix : listFixedBill) {
             Row<PDPage> row = table.createRow(10f);
-            cell = row.createCell(10, String.valueOf(count++));
-            cell = row.createCell(100, penjualan.getName());
-            cell = row.createCell(30, String.valueOf(penjualan.getStock()));
-            cell = row.createCell(40, String.valueOf(penjualan.getPrice()));
-            cell = row.createCell(40, String.valueOf(penjualan.getBuyPrice()));
-            cell = row.createCell(90, penjualan.getCategory());
+            cell = row.createCell(5, String.valueOf(count++));
+            cell = row.createCell(30, penjualanFix.getName());
+            cell = row.createCell(10, String.valueOf(penjualanFix.getStock()));
+            cell = row.createCell(15, String.valueOf(penjualanFix.getPrice()));
+            cell = row.createCell(15, String.valueOf(penjualanFix.getBuyPrice()));
+            cell = row.createCell(20, penjualanFix.getCategory());
         }
         table.draw();
 
@@ -83,7 +91,7 @@ public class LaporanFixedBill extends Laporan{
 
         contentStream.close();
         document.addPage(page);
-        document.save(new File(getFileName()));
+        document.save(new File(System.getProperty("user.dir") + "/docs/" + getFileName()));
         document.close();
     }
 }
