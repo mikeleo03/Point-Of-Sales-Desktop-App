914fbd970b78198af8725d3f483560ab3a23077b
Michael Leon Putra Widhi <leonmichael463@gmail.com>
2023-05-07 19:30:50 +0700

    add interfaces to gather data

--- src/main/java/main/Pages/DashboardPage/InterfacePage.java
+++ src/main/java/main/Pages/DashboardPage/InterfacePage.java
@@ -5,13 +5,15 @@ import java.util.HashMap;
 import javax.swing.ImageIcon;
 import java.util.ArrayList;
 import javax.swing.JButton;
+import org.javatuples.*;
 
 public interface InterfacePage {
     // Function to add new page selection to main
     public void addPage(String pagename, ImageIcon imageicon, JPanel panel);
 
     // Function to get data from inventory
-    // public ArrayList<Tuple> getInventoryData();
+    public ArrayList<Septet<Integer, String, Integer, Double, Double, String, String>> getInventoryData();
 
-    // public ArrayList getClientManagerData();
+    // Function to get data from client manager
+    public HashMap<String,ArrayList<Quintet<Integer, String, String, Integer, Boolean>>> getClientManagerData();
 }
