81ac79148d17225e7dab9d364b0c67378c278bdc
AustinPardosi <gabrielpardosi26@gmail.com>
2023-05-07 18:41:17 +0700

    feat: add Plugin Page

---
+++ src/main/java/main/Pages/PluginPage/PluginPanel.java
@@ -0,0 +1,10 @@
+package main.Pages.PluginPage;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class PluginPanel extends JPanel {
+    public PluginPanel() {
+        setBackground(new Color(0x1a1e3b));
+    }
+}
